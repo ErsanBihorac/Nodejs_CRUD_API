62e0755b80aeffcd2cef9f25f1b8ae65af0da672
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-04-26 03:46:58 +0200

    get and post request done

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,1 +1,23 @@
-module.exports = (req, res) => { };
+const crypto = require('crypto');
+const requestBodyparser = require('../util/body-parser');
+module.exports = async (req, res) => {
+    if (req.url === 'api/movies') {
+        try {
+            let body = await requestBodyparser(req);
+            console.log('Request Body: ', body);
+            body.id = crypto.randomUUID();
+            req.movies.push(body);
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end();
+        } catch (err) {
+            console.log(err);
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end(
+                JSON.stringify({
+                    title: 'Validation Failed',
+                    message: 'Request body is not valid'
+                })
+            );
+        }
+    }
+};
